1a531c193dccc30ac7fb025d38c3ccc581befb36
Vlad Hrybok <vgribok@hotmail.com>
2021-12-07 17:03:40 -0500

    Added cdk-shared and BetterStack

--- src/SqsLongDelay/SqsLongDelayStack.cs
+++ src/SqsLongDelay/SqsLongDelayStack.cs
@@ -1,13 +1,27 @@
-using Amazon.CDK;
+#nullable enable
+using CdkShared;
 using Constructs;
 
 namespace SqsLongDelay
 {
-    public class SqsLongDelayStack : Stack
+    public class SqsLongDelayStack : BetterStack
     {
-        internal SqsLongDelayStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        private static string GetQueueName(Construct scope)
+            => scope.GetCtxString("QueueName", "long-retry-test-queue");
+
+        internal SqsLongDelayStack(Construct scope, string id = "SqsLongDelayStack", BetterStackProps? props = null) 
+            : base(scope, id, InitStackProps(props))
         {
             // The code that defines your stack goes here
+        }
+
+        private static BetterStackProps InitStackProps(BetterStackProps? props)
+        {
+            props ??= new BetterStackProps();
+
+            props.DynamicStackNameGenerator ??= scope => $"Retry-SQS--{GetQueueName(scope)}";
+
+            return props;
         }
     }
 }
