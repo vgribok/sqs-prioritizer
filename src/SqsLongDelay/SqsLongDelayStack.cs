2d50e198e7a4d6f66c46480a42a9196920d6fa18
Vlad Hrybok <vgribok@hotmail.com>
2021-12-07 16:22:16 -0500

    Initial commit

---
+++ src/SqsLongDelay/SqsLongDelayStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace SqsLongDelay
+{
+    public class SqsLongDelayStack : Stack
+    {
+        internal SqsLongDelayStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
