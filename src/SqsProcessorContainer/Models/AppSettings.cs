0a7253884d33768141204463f51cc41b4e820361
Vlad Hrybok <vgribok@hotmail.com>
2021-12-16 21:33:49 -0500

    Added support for .NET Secrets.

---
+++ src/SqsProcessorContainer/Models/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using Amazon;
+
+namespace SqsProcessorContainer.Models
+{
+    public class AppSettings
+    {
+        public string QeueueArn{ get; set; } = string.Empty;
+
+        public int ProcessorCount { get; set; }
+
+        public Arn QueueArnParsed => Arn.Parse(this.QeueueArn);
+    }
+}
--- src/SqsProcessorContainer/Program.cs
+++ src/SqsProcessorContainer/Program.cs
@@ -2,32 +2,44 @@
 
 // See https://aka.ms/new-console-template for more information
 
-using Amazon;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using SqsProcessorContainer;
+using SqsProcessorContainer.Models;
 
 public class Program : BackgroundService
 {
-    public static Task Main(string[] args) =>
-        CreateHostBuilder(args).Build().RunAsync();
+    public static Task Main(string[] args) => CreateHostBuilder(args).Build().RunAsync();
 
     static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
-            .ConfigureServices((_, services) =>
-                services.AddHostedService<Program>());
+            .ConfigureServices((context, services) => ConfigureServices(context, services));
 
+    private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+    {
+        services.AddHostedService<Program>();
+        // Add other services below
+
+        services.Configure<AppSettings>(context.Configuration.GetSection("AppSettings"));
+    }
+
+
+    private AppSettings Settings { get; }
+
+    public Program(IServiceProvider iocContainer, IOptions<AppSettings> settings)
+    {
+        Settings = settings.Value;
+    }
+    
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        const int listenerCount = 3;
-        Arn queueArn = Arn.Parse("arn:aws:sqs:us-east-2:444039259723:delme");
-
-        List<NopMessageProcessor> listeners = SqsProcessor<MessageModel>.StartProcessors(listenerCount,
-            (i) => new NopMessageProcessor(queueArn, i.ToString())
+        List<NopMessageProcessor> listeners = SqsProcessor<MessageModel>.StartProcessors(this.Settings.ProcessorCount,
+            (i) => new NopMessageProcessor(this.Settings.QueueArnParsed, i.ToString())
         );
 
         IEnumerable<Task> listenerTasks = listeners.Select(l => l.Listen(stoppingToken));
-
         return Task.WhenAll(listenerTasks);
     }
 }
